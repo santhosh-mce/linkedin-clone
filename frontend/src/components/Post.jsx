757633be3fd28447c047d658348ea3cec1b3021d
Santhosh1507 <webdesign1507@gmail.com>
2024-09-15 10:38:54 +0530

    first commit

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -216,13 +216,19 @@ const Post = ({ post }) => {
       )}
 
       {isImagePopupOpen && (
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
-          <img
-            src={popupImage}
-            alt='Full-size'
-            className='max-w-full max-h-full cursor-pointer'
-            onClick={closeImagePopup}
-          />
+        <div className='fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50'>
+          <div className='bg-white p-4 rounded-lg relative'>
+            <div className=''>
+              <img src={popupImage} alt='Popup' className='lg:max-w-4xl md:max-w-xl sm:max-w-md max-h-full' />
+
+            </div>
+            <button
+              onClick={closeImagePopup}
+              className='absolute -top-2 right-0 text-stone-900 text-3xl sm:text-lg md:text-3xl lg:text-4xl'
+            >
+              &times;
+            </button>
+          </div>
         </div>
       )}
 
